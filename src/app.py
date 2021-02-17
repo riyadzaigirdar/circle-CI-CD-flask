3e0211d02730cab0b76b062a30d3ba9aab9b1967
riyadzaigirdar <riyad1401043@gmail.com>
2021-02-18 00:13:37 +0600

    c

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Riyad did CI/CD Alhamdulillah"
+    return "Hello Riyad did CI/CD Alhamdulillah Alhamdulillah"
 
 if __name__ == "__main__":
     app.run()
